ee22f156bcd6924cf6680c3dbaef6c7995fcc478
David Mora <davidnmora@gmail.com>
2023-02-06 12:25:27 -0800

    Update to lyric data through 2022 (and re-run pipeline)

--- 7-convert-data-to-deepscatter-format.sh
+++
@@ -1,5 +0,0 @@
-# launch python 3.9+ virtual env
-workon python3.9env
-
-# do it!
-sudo quadfeather --files 6-OUTPUT-lyric-lines-quadfeather-ready.csv --tile_size 10000 --destination deepscatter-tiles
---
+++ convert-data-to-deepscatter-format.sh
@@ -0,0 +1,5 @@
+# launch python 3.9+ virtual env
+workon python3.9env
+
+# do it!
+sudo quadfeather --files 7-OUTPUT-lyric-lines-quadfeather-ready.csv --tile_size 10000 --destination deepscatter-tiles
